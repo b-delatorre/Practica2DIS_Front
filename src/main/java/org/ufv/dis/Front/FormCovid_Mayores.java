cc34dc1ba35d170525f6cb10e032ad2b3a00bacf
Guillermo <9004315@alumnos.ufv.es>
2023-01-11 20:25:13 +0100

    Improvements into UI

---
+++ src/main/java/org/ufv/dis/Front/FormCovid_Mayores.java
@@ -0,0 +1,39 @@
+package org.ufv.dis.Front;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+
+import java.awt.*;
+import java.text.DateFormat;
+import java.util.Date;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+
+public class FormCovid_Mayores extends FormLayout {
+    private TextField code=new TextField("codigo_geometria");
+    private TextField zona=new TextField("zona_basica_salud");
+    private TextField catorcedias=new TextField("Incidencia_14_dias");
+    private TextField incidenciaTotal=new TextField("Incidencia_Total");
+    private TextField TotalCasos=new TextField("Total_Casos");
+    private TextField CasosCatorce=new TextField("Casos_14_dias");
+    private Button Aceptar=new Button("Aceptar");
+    private Button Cancelar=new Button("Delete");
+    private MainView myUI;
+
+    public FormCovid_Mayores(MainView myUI){
+        this.myUI=myUI;
+        setSizeUndefined();
+
+        Aceptar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        Aceptar.addClickShortcut(Key.ENTER);
+        Cancelar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        Cancelar.addClickShortcut(Key.ENTER);
+        //add(CasosCatorce);
+        //add(code,zona,catorcedias,incidenciaTotal,TotalCasos,CasosCatorce);
+    }
+}
--- src/main/java/org/ufv/dis/Front/MainView.java
+++ src/main/java/org/ufv/dis/Front/MainView.java
@@ -2,35 +2,32 @@ package org.ufv.dis.Front;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.server.VaadinRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-import java.util.ArrayList;
-import java.util.stream.Stream;
 
 
-@Route
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+@Route
+public class MainView extends VerticalLayout{
 
-public class MainVIew extends VerticalLayout{
-
-    public MainVIew(@Autowired CovidService service){
+    public MainView(@Autowired CovidService service){
         HorizontalLayout inputs=new HorizontalLayout();
         VerticalLayout results=new VerticalLayout();
+        //FormCovid_General formulario_general=new FormCovid_General(this);
+        //FormCovid_Mayores formulario_mayores=new FormCovid_Mayores(this);
         ComboBox<String> comboBox=new ComboBox<>("Seleccione el tipo de info....");
         comboBox.setAllowCustomValue(false);
         comboBox.setItems("Tasa acumulada poblacion general", "Tasa acumulada poblacion mayores de 65");
         comboBox.setHelperText("Seleccione el tipo de dato");
+        inputs.add(comboBox);
 
         Grid<Data> grid_General= new Grid<>(Data.class,true);
         grid_General.addColumn(Data::getCasos).setHeader("Casos");
@@ -45,7 +42,7 @@ public class MainVIew extends VerticalLayout{
         grid_Mayor.addColumn(DataMayor::getFecha).setHeader("Fecha");
         grid_Mayor.addColumn(DataMayor::getTasa14).setHeader("Tasa 14 dias");
         grid_Mayor.addColumn(DataMayor::getZona).setHeader("Zona");
-        inputs.add(comboBox);
+
         Button boton1=new Button("Lee caracter",
         e -> {
             String tipoPeticion=comboBox.getValue();
@@ -53,8 +50,8 @@ public class MainVIew extends VerticalLayout{
             try {
                 results.removeAll();
                 if(tipoPeticion.equals("Tasa acumulada poblacion general")){
-                   grid_General.setItems(service.leeCovidMenor());
-                   results.add(grid_General);
+                    grid_General.setItems(service.leeCovidMenor());
+                    results.add(grid_General);
                 } else if (tipoPeticion.equals("Tasa acumulada poblacion mayores de 65")) {
                     grid_Mayor.setItems(service.leeCovidMayor());
                     results.add(grid_Mayor);
@@ -66,8 +63,8 @@ public class MainVIew extends VerticalLayout{
         });
         boton1.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         boton1.addClickShortcut(Key.ENTER);
-
-        addClassName("centered-content");
+        //addClassName("centered-content");
         add(inputs,boton1,results);
+
     }
 }
